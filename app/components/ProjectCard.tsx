6ebf3eed1788b163e54b0c5dbb2dd477191511ac
James Graham <me@jamesgraham.co.uk>
2024-11-11 17:09:45 +0000

    make project cards prettier

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -19,15 +19,17 @@ export default function ProjectCard({ project }: { project: Project }) {
           <span className="absolute -top-10 -left-1 font-bold">
             {project.startDate.slice(0, 4)}
           </span>
-          <div className="flex-1 pl-2 h-full flex flex-col">
-            <div className="overflow-hidden relative h-52 sm:h-60 flex flex-col">
-              <div className="z-10 flex justify-between items-center pr-2">
-                <h2 className="font-bold my-2">{project.title}</h2>
+          <div className="flex-1 px-2 h-full flex flex-col">
+            <div className="relative overflow-hidden h-52 sm:h-60 flex flex-col">
+              <div className="z-10 flex justify-between items-center pb-2">
+                <h2 className="my-2 text-black text-lg uppercase leading-tight px-1 group-hover:bg-black group-hover:text-white">
+                  {project.title}
+                </h2>
                 <ProjectStageChip stage={project.stage} />
               </div>
               {image && (
                 <Image
-                  className="project-card-image object-cover w-full h-full md:opacity-85 transition-all duration-500 sm:group-hover:scale-105 sm:group-hover:opacity-100"
+                  className="project-card-image object-cover w-full h-full md:opacity-65 transition-all duration-500 sm:group-hover:scale-105 sm:group-hover:opacity-100"
                   src={image.url}
                   alt={image.title}
                   width={500}
@@ -35,20 +37,22 @@ export default function ProjectCard({ project }: { project: Project }) {
                 />
               )}
             </div>
-            <p className="pt-4 pb-2 text-sm sm:text-base">
-              {project.description}
-            </p>
-            <p className="py-2 text-xs sm:text-sm border-t border-dashed mt-auto">
-              {project.technologies.map((tech, index) => (
-                <span key={tech}>
-                  {tech}
-                  {index < project.technologies.length - 1 ? " · " : ""}
-                </span>
-              ))}
-            </p>
+            <div className="mt-2 flex flex-col flex-1">
+              <p className="py-2 text-sm sm:text-base border-t border-dashed border-gray-300">
+                {project.description}
+              </p>
+              <p className="py-2 text-xs font-light sm:text-sm mt-auto border-t border-dashed border-gray-300">
+                {project.technologies.map((tech, index) => (
+                  <span key={tech}>
+                    {tech}
+                    {index < project.technologies.length - 1 ? " · " : ""}
+                  </span>
+                ))}
+              </p>
+            </div>
           </div>
           <div className="w-8 h-auto relative border-l border-gray-300 group-hover:bg-black group-hover:text-white">
-            <span className="w-24 rotate-90 origin-left text-nowrap flex translate-x-4 uppercase tracking-widest font-medium text-sm">
+            <span className="w-24 rotate-90 origin-left text-nowrap flex translate-x-4 uppercase tracking-widest text-sm">
               view more
             </span>
           </div>
--- app/components/ProjectStageChip.tsx
+++ app/components/ProjectStageChip.tsx
@@ -1,7 +1,7 @@
 export default function ProjectStageChip({ stage }: { stage: string }) {
   return (
     <span
-      className={`rounded px-1 text-white text-sm font-medium shadow max-w-fit ${
+      className={`px-1 text-white text-sm font-medium shadow max-w-fit ${
         stage === "SHIPPED" ? "bg-green-400" : "bg-gray-700"
       }`}
     >
